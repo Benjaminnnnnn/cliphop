5771369339278af958175a33abf22463284119a8
Benjamin Zhuang <zhuanz3@uw.edu>
2023-09-06 14:18:42 -0700

    removed app comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,14 +21,6 @@ export default function App({ Component, pageProps }: AppProps) {
       clientId={`${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`}
     >
       <div className="h-screen w-screen overflow-x-hidden">
-        {/* <Navbar></Navbar> */}
-        {/* <div className="mt-4 flex gap-6 md:gap-20">
-          <div className="ml-4 h-[92vh] overflow-hidden xl:hover:overflow-auto">
-            <Sidebar></Sidebar>
-          </div> */}
-
-        {/* main content */}
-        {/* <div className="videos flex h-[88vh] flex-1 flex-col gap-10 overflow-auto"> */}
         <AnimatePresence initial={false}>
           <motion.div
             className="videos flex h-full flex-1 flex-col overflow-auto"
@@ -41,7 +33,6 @@ export default function App({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </motion.div>
         </AnimatePresence>
-        {/* </div> */}
       </div>
     </GoogleOAuthProvider>
   );
